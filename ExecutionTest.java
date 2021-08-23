1d8b8b4d2eaad6fe835514b3c2d0103d8819e445
dinesh <dinesh@codingmart.com>
2021-08-23 16:33:06 +0530

    First Commit

---
+++ ExecutionTest.java
@@ -0,0 +1,16 @@
+package execution.time;
+
+public class ExecutionTest {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i <= 100_000; i++) {
+            System.out.println("LOOPED: " + i);
+        }
+
+        long end = System.currentTimeMillis();
+
+        System.out.println((end-start) / 1000);
+
+    }
+}
